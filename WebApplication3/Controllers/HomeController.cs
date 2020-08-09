fa785d413022e7b85f70996e92231a4c9d45d11f
MUHAMMAD AIMAN HAKIM BIN MOHD SALIM MUHAMMAD AIMAN HAKIM BIN MOHD SALIM <Aimannnhakim@gmail.com>
2020-08-09 23:34:28 +0800

    Add project files.

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Main()
+        {
+            return View();
+        }
+
+
+
+        public ActionResult DrawChart()
+        {
+        
+            var controller = DependencyResolver.Current.GetService<InventoryController>();
+            controller.ControllerContext = new ControllerContext(this.Request.RequestContext,
+            controller);
+
+            return View("~/Views/Inventory/Index.cshtml",controller);
+        }
+        
+
+
+    }
+}
---
+++ WebApplication3/Controllers/InventoryController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+
+namespace WebApplication3.Controllers
+{
+    public class InventoryController : Controller
+    {
+        // GET: Inventory
+        public ActionResult Index()
+        {
+            int aexpired = AlreadyExpired();
+            int nexpired = OtwExpired();
+
+            string [] labels = { "Expired", "Not Expired" };
+            int[] values = { AlreadyExpired(), OtwExpired()};
+
+            ViewBag.lbl =labels;
+            ViewBag.vlu = values;
+
+            return View();
+        }
+
+        //create data object
+        Models.Inventory item1 = new Models.Inventory { Id = 1, Exp = 0 };
+        Models.Inventory item2 = new Models.Inventory { Id = 2, Exp = 10 };
+        Models.Inventory item3 = new Models.Inventory { Id = 3, Exp = 5 };
+        Models.Inventory item4 = new Models.Inventory { Id = 4, Exp = 0 };
+
+
+        //insert object into list
+        public List<Models.Inventory> ListItem()
+        {
+
+            List<Models.Inventory> Item = new List<Models.Inventory>();
+            Item.Add(item1);
+            Item.Add(item2);
+            Item.Add(item3);
+            Item.Add(item4);
+
+            return Item;
+        }
+
+        //count expired tool
+        public int AlreadyExpired() //Exp day = 0
+        {
+            var List = ListItem();
+            var Expired = List.FindAll(item => item.Exp == 0);
+
+            return Expired.Count;
+        }
+
+        //count non expired tool
+        public int OtwExpired() //Exp day > 0
+        {
+            var List = ListItem();
+            var Expired = List.FindAll(item => item.Exp > 0);
+
+            return Expired.Count;
+        }
+
+
+    }
+
+    
+}
